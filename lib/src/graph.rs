8755d9a8d9d12717583b44c2e96f90d4c1d8ead0
D-McInnes8 <96274982+D-McInnes8@users.noreply.github.com>
2023-12-29 18:43:31 +1000

    Added bounds checking to graph functions

--- lib/src/graph.rs
+++ lib/src/graph.rs
@@ -16,6 +16,11 @@ pub struct Graph {
     edges: Vec<EdgeData>,
 }
 
+#[derive(Debug, PartialEq)]
+pub enum GraphOperationError {
+    NodeDoesNotExist,
+}
+
 impl Graph {
     pub fn new() -> Graph {
         Graph {
@@ -33,7 +38,16 @@ impl Graph {
         return index;
     }
 
-    pub fn add_edge(&mut self, source: NodeIndex, target: NodeIndex, weight: i32) {
+    pub fn add_edge(
+        &mut self,
+        source: NodeIndex,
+        target: NodeIndex,
+        weight: i32,
+    ) -> Result<(), GraphOperationError> {
+        if source >= self.nodes.len() || target >= self.nodes.len() {
+            return Err(GraphOperationError::NodeDoesNotExist);
+        }
+
         let edge_index = self.edges.len();
         let node_data = &mut self.nodes[source];
 
@@ -43,9 +57,14 @@ impl Graph {
         });
 
         node_data.edges.push(edge_index);
+        return Ok(());
     }
 
     pub fn get_edge_weight(&self, source: NodeIndex, target: NodeIndex) -> Option<i32> {
+        if source >= self.nodes.len() || target >= self.nodes.len() {
+            return None;
+        }
+
         for edge in &self.nodes[source].edges {
             let edge_data = &self.edges[*edge];
             if edge_data.target == target {
@@ -79,7 +98,7 @@ mod tests {
         let n0 = graph.add_node(1);
         let n1 = graph.add_node(2);
 
-        graph.add_edge(n0, n1, 5);
+        _ = graph.add_edge(n0, n1, 5);
 
         assert_eq!(graph.get_edge_weight(n0, n1), Some(5));
         assert_eq!(graph.get_edge_weight(n1, n0), None);
@@ -94,10 +113,10 @@ mod tests {
         let n2 = graph.add_node(3);
         let n3 = graph.add_node(4);
 
-        graph.add_edge(n0, n1, 6);
-        graph.add_edge(n1, n0, 5);
-        graph.add_edge(n2, n1, 2);
-        graph.add_edge(n3, n0, 10);
+        _ = graph.add_edge(n0, n1, 6);
+        _ = graph.add_edge(n1, n0, 5);
+        _ = graph.add_edge(n2, n1, 2);
+        _ = graph.add_edge(n3, n0, 10);
 
         assert_eq!(graph.get_edge_weight(n0, n1), Some(6));
         assert_eq!(graph.get_edge_weight(n1, n0), Some(5));
@@ -105,5 +124,21 @@ mod tests {
         assert_eq!(graph.get_edge_weight(n1, n2), None);
         assert_eq!(graph.get_edge_weight(n3, n0), Some(10));
         assert_eq!(graph.get_edge_weight(n0, n3), None);
+    }
+
+    #[test]
+    fn add_invalid_edges_no_nodes() {
+        let mut graph = Graph::new();
+
+        let n0 = graph.add_node(1);
+        let n1 = graph.add_node(2);
+        let n2 = n1 + 1;
+
+        let e0 = graph.add_edge(n2, n1, 4);
+        let e1 = graph.add_edge(n0, n2, 7);
+
+        assert_eq!(e0, Err(GraphOperationError::NodeDoesNotExist));
+        assert_eq!(e1, Err(GraphOperationError::NodeDoesNotExist));
+        assert_eq!(graph.get_edge_weight(n0, n2), None);
     }
 }
