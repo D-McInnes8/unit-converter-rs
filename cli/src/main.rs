c989359aeb45f9aeed061f39943000430752228b
D-McInnes8 <96274982+D-McInnes8@users.noreply.github.com>
2023-12-31 19:10:01 +1000

    Created CLI app that will read from standard input

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(len) => {
+                if len == 0 {
+                    return;
+                } else {
+                    let command = remove_new_line_characters(&input);
+                    if command == "exit" {
+                        return;
+                    }
+                    println!("{}", command);
+                }
+            }
+            Err(error) => {
+                eprintln!("error: {}", error);
+                return;
+            }
+        }
+    }
+}
+
+fn remove_new_line_characters(input: &String) -> &str {
+    let result = input
+        .strip_suffix("\r\n")
+        .or(input.strip_suffix("\n"))
+        .unwrap_or(input);
+    return result;
 }
