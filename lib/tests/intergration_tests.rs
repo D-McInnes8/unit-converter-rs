4c86bab2d3002e788619932a47164b1a2586ff42
D-McInnes8 <96274982+D-McInnes8@users.noreply.github.com>
2024-01-07 12:49:58 +1000

    Added test cases support for intergration tests

--- lib/tests/intergration_tests.rs
+++ lib/tests/intergration_tests.rs
@@ -1,13 +1,26 @@
 use self::common::setup;
+use test_case::test_case;
 
 mod common;
 
-#[test]
+#[test_case("2k -> m",    2000.0            ; "kilometers to meters")]
+#[test_case("1k -> m",    1000.0            ; "meters to kilometers")]
+#[test_case("2.5k -> mi", 1.553431841624517 ; "kilometers to miles")]
+#[test_case("2km -> nmi", 1.079913606911447 ; "kilometers to nautical miles")]
+pub fn valid_conversion(input: &str, expected: f64) {
+    let mut converter = setup();
+    let actual = converter.convert_from_expression(input);
+
+    assert!(actual.is_ok(), "Returned error {:?}", actual.err());
+    assert_eq!(expected, actual.unwrap());
+}
+
+/*#[test]
 pub fn kilometers_to_meters() {
     let mut converter = setup();
     let actual = converter.convert_from_expression("2k -> m");
     assert_eq!(actual.ok(), Some(2000.0))
-}
+}*/
 
 /*#[test]
 pub fn no_default_conversions() {
