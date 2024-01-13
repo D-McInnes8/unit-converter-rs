0d696e67407d9e7bedd4ea5197df4f4a06aa3a05
D-McInnes8 <96274982+D-McInnes8@users.noreply.github.com>
2024-01-13 19:45:00 +1000

    Updated test cases in intergration tests

--- lib/tests/intergration_tests.rs
+++ lib/tests/intergration_tests.rs
@@ -5,12 +5,12 @@ mod common;
 
 #[test_case("2k -> m",           2000.0               ; "kilometers to meters")]
 #[test_case("1k -> m",           1000.0               ; "meters to kilometers")]
-#[test_case("2.5k -> mi",        1.553431841624517    ; "kilometers to miles")]
+#[test_case("2.5k -> mi",        1.5534318416245172   ; "kilometers to miles")]
 #[test_case("2km -> nmi",        1.0799136069114472   ; "kilometers to nautical miles")]
 #[test_case("2.092333nmi -> m",  3875.000716          ; "nautical miles to meters")]
 #[test_case("1cm -> km",         0.00001              ; "centermeters to kilometers")]
 #[test_case("3.27km -> nm",      3270000000000.0      ; "kilometers to nanometers")]
-#[test_case("453406564nm -> km", 0.000453406564       ; "nanometers to kilometers")]
+#[test_case("453406564nm -> km", 0.0004534065640000001; "nanometers to kilometers")]
 #[test_case("87pm -> nm",        0.087000000000000001 ; "picometers to nanometers")]
 #[test_case("1ly -> km",         9460730472580.0      ; "light-years to kilometers")]
 pub fn length_conversion(input: &str, expected: f64) {
