6342de56205d289c01bec239f3d87a305cb6b2fd
D-McInnes8 <96274982+D-McInnes8@users.noreply.github.com>
2024-04-28 13:30:44 +1000

    Added additional intergration tests to the unit-converter crate for negative numbers

--- crates/unit-converter/tests/integration_tests.rs
+++ crates/unit-converter/tests/integration_tests.rs
@@ -21,7 +21,23 @@ pub fn length_conversion(input: &str, expected: f64) {
     for _ in 0..2 {
         let actual = converter.convert_from_expression(input);
 
-        eprintln!("VALUE: {:?}", &actual);
+        assert!(actual.is_ok(), "Returned error {:?}", actual.err());
+        assert_eq!(expected, actual.unwrap().value);
+    }
+}
+
+#[test_case("-5km -> m",       -5000.0           ; "kilometers to meters")]
+#[test_case("-0.06m -> nm",    -60000000.0       ; "meters to nanometers")]
+#[test_case("-0.06nmi -> km",  -0.11112          ; "nautical miles to kilometers")]
+#[test_case("-7.89e1mi -> m",  -126976.926       ; "miles in e-notation to meters")]
+#[test_case("-5.6e-5nm -> pm", -0.056            ; "nanometers in e-notation to picometers")]
+#[test_case("-13C -> F",       8.600000000000001 ; "celsius to fahrenheit")]
+pub fn negative_numbers(input: &str, expected: f64) {
+    let mut converter = setup();
+
+    // Run each test case twice to ensure that any caching doesn't alter the result.
+    for _ in 0..2 {
+        let actual = converter.convert_from_expression(input);
 
         assert!(actual.is_ok(), "Returned error {:?}", actual.err());
         assert_eq!(expected, actual.unwrap().value);
@@ -31,8 +47,6 @@ pub fn length_conversion(input: &str, expected: f64) {
 #[test_case("1.079913e9km -> nmi", 583106371.4902809          ; "kilometers to nautical miles")]
 #[test_case("9.9999e4km -> m",     99999000.0                 ; "kilometers to meters")]
 #[test_case("4.5e-2ly -> nm",      425732871266100000000000.0 ; "light-years to nanometers")]
-#[test_case("-7.89e1mi -> m",      -126976.926                ; "miles to meters")]
-#[test_case("-5.6e-5nm -> pm",     -0.056                     ; "nanometers to picometers")]
 pub fn e_notation(input: &str, expected: f64) {
     let mut converter = setup();
 
